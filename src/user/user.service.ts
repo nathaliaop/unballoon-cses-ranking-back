3fb7ca9df4901cfbb74e2b259e72e69da94d6bc6
Nathália Pereira <nathaliao@lacunasoftware.com>
2023-01-21 02:59:53 -0300

    Adds data from web scraping to the API

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,50 +1,58 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { google } from 'googleapis';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 @Injectable()
 export class UserService {
+  getUserInfoCSES = (userId: string) => {
+    const promise = axios.get(`${process.env.API_CSES}/user/${userId}`);
+
+    const userInfo = promise
+      .then((response) => cheerio.load(response.data))
+      .then(($) => {
+        let handle = $('.title-block').text().slice(1);
+        handle = handle.slice(handle.indexOf('User') + 5, handle.indexOf('\n'));
+
+        const numberOfQuestions = $(`a[href*=${userId}]`).text();
+
+        return {
+          id: userId,
+          username: handle,
+          numberOfQuestions: Number(numberOfQuestions),
+        };
+      });
+
+    return userInfo;
+  };
+
   async readSheet() {
-    // try {
+    try {
+      const sheets = google.sheets({
+        version: 'v4',
+        auth: await google.auth.getClient({ scopes: [process.env.SCOPE] }),
+      });
 
-    const sheets = google.sheets({
-      version: 'v4',
-      auth: await google.auth.getClient({ scopes: [process.env.SCOPE] }),
-    });
+      const res = await sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.SHEET_ID,
+        range: 'Unballoon!C:C',
+      });
 
-    const res = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SHEET_ID,
-      range: 'Unballoon!A:C',
-    });
+      console.log(res.data.values);
 
-    // const [handle_cses, handle_codeforces, id] = res.data.values;
+      const rows = res.data.values.slice(1);
 
-    return res.data.values;
+      const promises = [];
 
-    // const fields = res.data.values[0];
+      rows.map(async (row) => {
+        const userId = row[0];
 
-    // res.data.values.splice(0, 1);
+        promises.push(this.getUserInfoCSES(userId));
+      });
 
-    // const rows = res.data.values;
-
-    //   if (rows.length) {
-    //     const result = [];
-
-    //     rows.map((row) => {
-    //       const data = {};
-
-    //       fields.map((field, index) => {
-    //         data[field] = row[index];
-    //       });
-
-    //       result.push(data);
-    //     });
-
-    //     return result;
-    //   } else {
-    //     throw new HttpException('No data found.', 404);
-    //   }
-    // } catch (err) {
-    //   throw new HttpException('The API returned an error: ' + err, 500);
-    // }
+      return await Promise.all(promises);
+    } catch (err) {
+      throw new HttpException('The API returned an error: ' + err, 500);
+    }
   }
 }
